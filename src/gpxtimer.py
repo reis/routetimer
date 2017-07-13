86074fcbad5c1c5b99904d857ac3ff5a430f5dfc
André Reis <andre.reis@gmail.com>
2017-07-13 11:05:28 +0100

    More comments

--- src/gpxtimer.py
+++ src/gpxtimer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # pylint: disable=line-too-long
 # pylint: disable-msg=C0103
-""" This script updates a TCX file adjusting the timestamps
+""" This script updates a GPX file adding timestamps based on a simple chronograph data
 """
 from lxml import etree
 import datetime
 import time
 from math import radians, cos, sin, sqrt
 
+# Constant to used to calculate the distance between two points
 EARTH_RADIUS = 6370000
 
 # New data to create a new run with a existent route
@@ -47,6 +48,7 @@ xfirstpointtime = etree.Element('time')
 xfirstpointtime.text = datetime.datetime.strptime(nstarttime, '%Y-%m-%d %H:%M').strftime('%Y-%m-%dT%H:%M:%S+00:00')
 xfirstpoint.append(xfirstpointtime)
 
+# Insert time in the metadata to be used like an ID
 xmetadatatime = etree.Element('time')
 xmetadatatime.text = datetime.datetime.strptime(nstarttime, '%Y-%m-%d %H:%M').strftime('%Y-%m-%dT%H:%M:%S+00:00')
 xmetadata.append(xmetadatatime)
@@ -62,7 +64,7 @@ for split in nsplits:
     lastsplitdistance = float(split['distance'])
     lastsplitseconds = split['seconds']
 
-# Insert time for all points
+# Set variables with previous element for the loop
 partialdistance = 0
 lasttime = xfirstpointtime.text
 lastlat = float(xfirstpoint.attrib['lat'])
@@ -70,11 +72,14 @@ lastlon = float(xfirstpoint.attrib['lon'])
 lastele = EARTH_RADIUS + float(xfirstpoint.xpath('n:ele', namespaces=nsmap)[0].text)
 currentsplit = 0
 
+# Loop all elements begining on second on to set the time for all points
 for xpoint in xtrackpoints[1:]:
+    # Get the current point data
     xpointlat = float(xpoint.attrib['lat'])
     xpointlon = float(xpoint.attrib['lon'])
     xpointele = EARTH_RADIUS + float(xpoint.xpath('n:ele', namespaces=nsmap)[0].text)
 
+    # Calculate the distance between two geographic points including altitude
     x0 = xpointele * cos(radians(xpointlat)) * sin(radians(xpointlon))
     y0 = xpointele * sin(radians(xpointlat))
     z0 = xpointele * cos(radians(xpointlat)) * cos(radians(xpointlon))
@@ -84,14 +89,17 @@ for xpoint in xtrackpoints[1:]:
     dist = sqrt((x1-x0)**2 + (y1-y0)**2 + (z1-z0)**2)
     partialdistance += dist
 
+    # Move the to the next split if necessary to get the right pace
     if partialdistance > nsplits[currentsplit]:
         currentsplit += 1
 
+    # Create the time element inside the trkpoint
     xpointtime = etree.Element('time')
     xpointtime.text = (datetime.datetime.strptime(lasttime, '%Y-%m-%dT%H:%M:%S+00:00') + \
                        datetime.timedelta(seconds=dist * nsplits[currentsplit]['pace'] + 0.1)).strftime('%Y-%m-%dT%H:%M:%S+00:00')
     xpoint.append(xpointtime)
 
+    # Update the previous element variables
     lastlat = xpointlat
     lastlon = xpointlon
     lastele = xpointele
